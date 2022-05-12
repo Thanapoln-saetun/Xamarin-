1f44a9a83024da826c24aededcce2cad2cabc42e
Hack <102792578+Thanapoln-saetun@users.noreply.github.com>
2022-05-12 21:17:21 +0700

    Add files via upload

---
+++ MovieFav/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MovieFav.Services;
+using MovieFav.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("mttregular.otf", Alias = "mttregular")]
+[assembly: ExportFont("msregular.otf", Alias = "msregular")]
+[assembly: ExportFont("moregular.otf", Alias = "moregular")]
+[assembly: ExportFont("mrregular.otf", Alias = "mrregular")]
+
+namespace MovieFav
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            
+            DependencyService.Register<IDataStore<Models.Movie>, MovieDataStore>();
+            MainPage = new AppShell()
+            {
+                BackgroundColor = Color.Black,  
+            };
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MovieFav/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieFav.Models
+{
+    public class Movie
+    {
+        public static int Count { get; internal set; }
+        public int MovieId { get; set; }//>>>>> Key
+        public string MovieCode { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public string Type { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
